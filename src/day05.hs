beaa8e1328ca5ca46af923d6c4b01609fb6126de
Joseph Petitti <joseph@petitti.org>
2022-10-31 08:42:10 -0400

    Finish day 5 part 2

--- src/day05.hs
+++ src/day05.hs
@@ -1,6 +1,5 @@
 import System.Environment
 import FsHelpers
-import Debug.Trace (trace)
 
 fileNameFromArgs :: [String] -> String
 fileNameFromArgs [] = "inputs/day05.txt"
@@ -12,7 +11,7 @@ main = do
   ls <- fileToLines $ fileNameFromArgs args
   let nums = map (read :: String -> Int) (split (==',') (head ls))
   putStrLn $ "Part 1: " ++ show (part1 nums)
-  -- putStrLn $ "Part 2: " ++ show (part2 nums)
+  putStrLn $ "Part 2: " ++ show (part2 nums)
 
 replace :: Int -> a -> [a] -> [a]
 replace index newVal xs = as ++ newVal : tail bs
@@ -30,8 +29,19 @@ run (4:p1:remaining, input, output, full)  -- output position mode
   = run (remaining, input, output ++ [full!!p1], full)
 run (104:p1:remaining, input, output, full) -- output immediate mode
   = run (remaining, input, output ++ [p1], full)
-run ([_, _], _, _, full) = ([], [], [], full)
-run ([_, _, _], _, _, full) = ([], [], [], full)
+run ([_, _], input, output, full) = ([], input, output, full)
+run ([_, _, _], input, output, full) = ([], input, output, full)
+run (p0:p1:p2:remaining, input, output, full)
+  | p0 `mod` 100 == 5  = let a1 = if p1m p0 == 0 then full!!p1 else p1
+                             a2 = if p2m p0 == 0 then full!!p2 else p2
+                             nr = if a1 /= 0 then drop a2 full else remaining
+                         in run (nr, input, output, full)
+  | p0 `mod` 100 == 6  = let a1 = if p1m p0 == 0 then full!!p1 else p1
+                             a2 = if p2m p0 == 0 then full!!p2 else p2
+                             nr = if a1 == 0 then drop a2 full else remaining
+                         in run (nr, input, output, full)
+  where p1m n = n `div` 100 `mod` 10
+        p2m n = n `div` 1000 `mod` 10
 run (p0:p1:p2:p3:remaining, input, output, full)
   | p0 `mod` 100 == 1  = let a1 = if p1m p0 == 0 then full!!p1 else p1
                              a2 = if p2m p0 == 0 then full!!p2 else p2
@@ -40,6 +50,14 @@ run (p0:p1:p2:p3:remaining, input, output, full)
   | p0 `mod` 100 == 2  = let a1 = if p1m p0 == 0 then full!!p1 else p1
                              a2 = if p2m p0 == 0 then full!!p2 else p2
                              (nr, nf) = multInstruction (a1, a2, p3, remaining, full)
+                         in run (nr, input, output, nf)
+  | p0 `mod` 100 == 7  = let a1 = if p1m p0 == 0 then full!!p1 else p1
+                             a2 = if p2m p0 == 0 then full!!p2 else p2
+                             (nr, nf) = ltInstruction (a1, a2, p3, remaining, full)
+                         in run (nr, input, output, nf)
+  | p0 `mod` 100 == 8  = let a1 = if p1m p0 == 0 then full!!p1 else p1
+                             a2 = if p2m p0 == 0 then full!!p2 else p2
+                             (nr, nf) = equalsInstruction (a1, a2, p3, remaining, full)
                          in run (nr, input, output, nf)
   | otherwise = error $ "Illegal first instruction: " ++ show p0
   where p1m n = n `div` 100 `mod` 10
@@ -57,9 +75,24 @@ multInstruction (a1, a2, a3, remaining, full) =
       newFull = replace a3 (a1 * a2) full
   in (drop delta newFull, newFull) 
 
+ltInstruction :: (Int, Int, Int, [Int], [Int]) -> ([Int], [Int])
+ltInstruction (a1, a2, a3, remaining, full) = 
+  let delta = length full - length remaining
+      newFull = replace a3 (if a1 < a2 then 1 else 0) full
+  in (drop delta newFull, newFull) 
+
+equalsInstruction :: (Int, Int, Int, [Int], [Int]) -> ([Int], [Int])
+equalsInstruction (a1, a2, a3, remaining, full) = 
+  let delta = length full - length remaining
+      newFull = replace a3 (if a1 == a2 then 1 else 0) full
+  in (drop delta newFull, newFull) 
+
 solve :: [Int] -> [Int] -> ([Int], [Int])
 solve input memory = (output, afterMemory)
   where (_, _, output, afterMemory) = run (memory, input, [], memory)
 
 part1 :: [Int] -> Int
 part1 nums = last $ fst $ solve [1] nums
+
+part2 :: [Int] -> Int
+part2 nums = last $ fst $ solve [5] nums
