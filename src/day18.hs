b1e4ee807a363495fa4317bce1490bceb5853507
Joseph Petitti <joseph@petitti.org>
2022-11-30 13:36:51 -0800

    Start part 2

--- src/day18.hs
+++ src/day18.hs
@@ -16,10 +16,11 @@ type Coord = (Int, Int)
 type Chart = Map.Map Coord Char
 type Locs = Map.Map Char Coord
 data State = State
-  { _sxy    :: Coord
+  { _sxy    :: [Coord] -- robot positions
   , _sc     :: Chart
   , _skeys  :: Locs
   , _sdoors :: Locs
+  , _last   :: Int     -- last cost
   } deriving (Show, Eq, Ord)
 
 main :: IO ()
@@ -28,45 +29,37 @@ main = do
   ls   <- fileToLines $ fileNameFromArgs args
   let initialState = parse ls
   putStrLn (prettyPrint initialState)
-  putStrLn $ "Numkeys = " ++ show (Map.size (_skeys initialState))
-  putStrLn $ "Part 1: " ++ show (part1 initialState)
+  --putStrLn $ "Part 1 (This will take a long time): " ++ show (solve initialState)
+  let p2Init = toPart2State initialState
+  putStrLn (prettyPrint p2Init)
+  putStrLn $ "Part 2 (This will take a long time): " ++ show (solve p2Init)
 
 neighbors :: State -> [State]
-neighbors (State (x, y) chart keys doors) = trace (show (Map.size keys)) states
-  where paths  = mapMaybe (pathTo chart (x, y)) (Map.elems keys)
+neighbors (State robots chart keys doors _) = trace (show (Map.size keys)) states
+  where paths  = concatMap (\robot -> mapMaybe (pathTo chart robot) (Map.elems keys)) robots
         states = map toState paths
-        toState (_, ps) = State c chart'' keys' doors
-          where c       = last ps
-                k       = chart Map.! c
+        toState (price, start, ps) = State robots' chart'' keys' doors price
+          where end     = last ps
+                robots' = end : filter (/=start) robots
+                k       = chart Map.! end
                 keys'   = Map.delete k keys
-                chart'  = Map.insert c '.' chart  -- key space is empty now
+                chart'  = Map.insert end '.' chart  -- key space is empty now
                 chart'' = if Map.member (toUpper k) doors  -- open corresponding door if it exists
                            then Map.insert (doors Map.! toUpper k) '.' chart'
                            else chart'
 
 cost :: State -> State -> Int
-cost (State { _sxy = s1 }) (State s2 chart _ _) = fst $ fromJust $ pathTo chart s1 s2
+cost _ (State _ _ _ _ lastCost) = lastCost
 
-part1 :: State -> Maybe Int
-part1 s = case dijkstra neighbors cost (Map.null . _skeys) s of
+solve :: State -> Maybe Int
+solve s = case dijkstra neighbors cost (Map.null . _skeys) s of
     Nothing     -> Nothing
-    Just (x, _) -> Just x
+    Just (x, path) -> Just x
 
-parse :: [String] -> State
-parse ls = foldr folder (State (0, 0) Map.empty Map.empty Map.empty) coords
-  where ys = [0..(length ls - 1)]
-        xs = [0..(length (head ls) - 1)]
-        coords = [(x, y) | x <- xs, y <- ys]
-        folder (x, y) (State start chart keys doors) = State start' chart' keys' doors'
-          where t      = ls !! y !! x
-                t'     = if t == '@' then '.' else t
-                start' = if t == '@' then (x, y) else start
-                doors' = if isUpper t then Map.insert t (x, y) doors else doors
-                chart' = Map.insert (x, y) t' chart
-                keys'  = if isLower t then Map.insert t (x, y) keys else keys
-
-pathTo :: Chart -> Coord -> Coord -> Maybe (Int, [Coord])
-pathTo chart from (gx, gy) = aStar graphFunc (\_ _ -> 1) h (==(gx, gy)) from
+pathTo :: Chart -> Coord -> Coord -> Maybe (Int, Coord, [Coord])
+pathTo chart from (gx, gy) = case aStar graphFunc (\_ _ -> 1) h (==(gx, gy)) from of
+    Nothing     -> Nothing
+    Just (a, b) -> Just (a, from, b)
   where h (x, y)         = (abs gx - x) + (abs gy - y)
         graphFunc (x, y) = foldr isOpen HS.empty adjacent
           where adjacent = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
@@ -76,13 +69,40 @@ pathTo chart from (gx, gy) = aStar graphFunc (\_ _ -> 1) h (==(gx, gy)) from
                   | otherwise = hs
 
 prettyPrint :: State -> String
-prettyPrint (State start chart _ _) = intercalate "\n" ls ++ "\n"
+prettyPrint (State robots chart _ _ _) = intercalate "\n" ls ++ "\n"
   where list = Map.keysSet chart
         minX = fst $ minimumBy (\(a, _) (b, _) -> a `compare` b) list
         minY = snd $ minimumBy (\(_, a) (_, b) -> a `compare` b) list
         maxX = fst $ maximumBy (\(a, _) (b, _) -> a `compare` b) list
         maxY = snd $ maximumBy (\(_, a) (_, b) -> a `compare` b) list
         toChar coord
-          | coord == start = '@'
+          | coord `elem` robots = '@'
           | otherwise      = Map.findWithDefault ' ' coord chart
         ls = [[toChar (x, y) | x <- [minX..maxX]] | y <- [minY..maxY]]
+
+parse :: [String] -> State
+parse ls = foldr folder (State [] Map.empty Map.empty Map.empty 0) coords
+  where ys = [0..(length ls - 1)]
+        xs = [0..(length (head ls) - 1)]
+        coords = [(x, y) | x <- xs, y <- ys]
+        folder (x, y) (State start chart keys doors _) = State start' chart' keys' doors' 0
+          where t      = ls !! y !! x
+                t'     = if t == '@' then '.' else t
+                start' = if t == '@' then [(x, y)] else start
+                doors' = if isUpper t then Map.insert t (x, y) doors else doors
+                chart' = Map.insert (x, y) t' chart
+                keys'  = if isLower t then Map.insert t (x, y) keys else keys
+
+
+toPart2State :: State -> State
+toPart2State (State ir chart keys doors _) = State robots chart' keys doors 0
+    where (x, y) = head ir
+          robots = [(x - 1, y + 1), (x + 1, y + 1), (x - 1, y - 1), (x + 1, y - 1)]
+          chart' = foldr (`Map.insert` '#') chart 
+            [ (x, y)
+            , (x - 1, y)
+            , (x + 1, y)
+            , (x, y - 1)
+            , (x, y + 1)
+            ]
+
